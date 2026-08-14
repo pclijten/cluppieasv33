7e25c7e0130ace78b6f2ce8807ce50ca14d8722b
pclijten <pclijten@gmail.com>
2026-08-14 22:28:24 +0200

    Fix formatting and cleanup in teams.js

--- js/teams.js
+++ js/teams.js
@@ -1452,7 +1452,7 @@ function koppelTeamTab(v, tab){
       if (String(S.team.format) === vorm) return;
       await updateDoc(doc(db,'teams',S.teamId), {format: vorm});
       meld('Wedstrijdvorm: ' + vorm + ' tegen ' + vorm);
-    };
+    });
     v.querySelector('#verlaatTeam').onclick = async () => {
       if (!confirm('Weet je zeker dat je dit team wilt verlaten?')) return;
       await updateDoc(doc(db,'teams',S.teamId), {
