5e7ddc5cf136413aad5226a7933674e07c07f453
pclijten <pclijten@gmail.com>
2026-08-16 19:43:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
    zonder dat de coach alle tabbladen hoeft te sluiten.
    ================================================================ */
 
-const CACHE_VERSIE = 'cluppie-v20260816b';
+const CACHE_VERSIE = 'cluppie-v20260817a';
 
 /* Minimale schil die we bij installatie alvast klaarzetten. De overige
    bestanden (JS-modules, iconen, fonts van gstatic) worden vanzelf
